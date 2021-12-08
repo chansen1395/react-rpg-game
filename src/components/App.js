40b7a8a00282226ba5af3589664c4de36eb52caa
Connor Hansen <chansen13@georgefox.edu>
2021-12-08 10:15:24 -0800

    add initial app, connect firebase, whiteboard MVP/data

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App(){
+  return ( 
+    <Router>
+      <Header />
+      <Switch>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
